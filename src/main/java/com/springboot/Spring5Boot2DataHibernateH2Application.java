caa2fa678c2820dc2bb95e64b64b1ca3862e7361
mupezzuol <murillopezzuol@hotmail.com>
2019-05-05 20:07:38 -0300

    Subindo Projeto

---
+++ src/main/java/com/springboot/Spring5Boot2DataHibernateH2Application.java
@@ -0,0 +1,13 @@
+package com.springboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Spring5Boot2DataHibernateH2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Spring5Boot2DataHibernateH2Application.class, args);
+	}
+
+}
